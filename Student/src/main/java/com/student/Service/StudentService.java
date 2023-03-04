572adb7779883aa508f373f0501c5c26f8b39d77
bibekanandabose <bibekanandabose@localhost>
2023-03-04 17:55:50 +0530

    push all changes

--- Student/src/main/java/com/student/Service/StudentService.java
+++ Student/src/main/java/com/student/Service/StudentService.java
@@ -35,12 +35,54 @@ public class StudentService {
     	for (Student s1:allStudentList )
     	{
     	StudentModel m1=
-    		StudentModel.builder().firstName(s1.getFirstName())
+    		StudentModel.builder().
+    		firstName(s1.getFirstName()).age(s1.getAge()).lastName(s1.getLastName()).id(s1.getId())
     		.build();
     		listOfStudentModel.add(m1);
     	}
     	
 		return 	listOfStudentModel;
 	}
+    
+    
+    public StudentModel saveStudent(StudentModel studentInputModel)
+    {
+    	
+    	Student student=new Student();
+    	
+    	student=convertModelToEntity(studentInputModel);
+    	student=studentRepo.save(student);
+    	
+    	return convertEtoSingleModel(student);
+    
+    	
+    }
+
+  public  StudentModel convertEtoSingleModel(Student student) {
+		
+	return	StudentModel.builder()
+			.firstName(student.getFirstName())
+			.lastName(student.getLastName())
+			.age(student.getAge())
+			.id(student.getId())
+		.build();
+	  
+	  
+	}
+
+	public  Student convertModelToEntity(StudentModel studentInputModel) {
+		
+		
+		return Student.builder()
+				.firstName(studentInputModel.getFirstName())
+				.lastName(studentInputModel.getLastName())
+				.age(studentInputModel.getAge())
+				.build();
+		
+	
+	}
+    
+    
+    
 
 }
